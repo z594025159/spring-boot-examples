351ad05ce623f72df4344ea56e4d267dbb7eacbd
z594025159 <594025159@qq.com>
2018-09-21 10:43:38 +0800

    创建spring-boot项目

---
+++ src/main/java/cn/cdi/demo/SpringBootExamplesApplication.java
@@ -0,0 +1,12 @@
+package cn.cdi.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootExamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootExamplesApplication.class, args);
+	}
+}
